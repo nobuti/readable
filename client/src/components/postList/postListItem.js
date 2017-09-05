871e943e032a3eb80c74100a9177fd8322fc996d
nobuti <buti@nobuti.com>
2017-09-05 09:19:08 +0200

    Some style for post list item.

--- client/src/components/postList/postListItem.js
+++ client/src/components/postList/postListItem.js
@@ -4,18 +4,27 @@ import { Link } from 'react-router-dom';
 import { votePost, VOTE } from '../../actions';
 import format from 'date-fns/format';
 
+import './postItem.css';
+
 const PostListItem = (props) => {
   const { id, author, title, timestamp, comments, voteScore, votePost } = props;
 
   return (
-    <li>
-      <h2>
+    <li className='PostItem'>
+      <h2 className='PostItem-title'>
         <Link to={`/post/${id}`}>
           {title}
         </Link>
       </h2>
-      <div>Submitted by {author}, {format(timestamp, 'D MMM YYYY')}</div>
-      <div>{comments} comments</div>
+
+      <div className='PostItem-meta'>
+        <div>
+          Submitted by <span className='PostItem-author'>{author}</span>, {format(timestamp, 'D MMM YYYY')}
+        </div>
+
+        <div>{comments} comments</div>
+      </div>
+
       <div>
         <button onClick={(e) => {
           votePost(id, VOTE.UP);
